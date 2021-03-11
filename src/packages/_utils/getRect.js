591c47734ff4c63a0a8dcfbbcc23e12ba5d77850
LuoTao <799204419@qq.com>
2021-03-11 17:55:46 +0800

    init

---
+++ src/packages/_utils/getRect.js
@@ -0,0 +1,16 @@
+export function getRect(selector, all) {
+  return new Promise((resolve) => {
+    uni
+      .createSelectorQuery()
+      .in(this)
+      [all ? 'selectAll' : 'select'](selector)
+      .boundingClientRect((rect) => {
+        if (all && Array.isArray(rect) && rect.length) {
+          resolve(rect)
+        } else if (!all && rect) {
+          resolve(rect)
+        }
+      })
+      .exec()
+  })
+}
---
+++ src/packages/button/EButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button
+    @click.stop="handleTap"
+    :open-type="openType"
+    :loading="loading && !disabled"
+    :style="[buttonStyle]"
+    :class="[{ 'e-disabled': disabled }, plain ? `e-${type}-button-plain` : '', `e-${type}-button`, `e-${size}-button`]"
+    :hover-class="hoverClass"
+    class="e-button"
+  >
+    <text v-if="loading && !disabled" class="e-button_loading-text">{{ loadingText }}</text>
+    <slot v-else></slot>
+  </button>
+</template>
+
+<script>
+export default {
+    name: 'EButton',
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        
+    }
+}
+</script>
